205569981cc1bb23f8d3bb065b3ac0595b19ff4a
heemi_fazeree <heemi@softwareq.com>
2023-04-29 12:56:18 +0800

    Initial commitAdd project files.

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -5,7 +5,28 @@ public partial class ContactsPage : ContentPage
 	public ContactsPage()
 	{
 		InitializeComponent();
+
+        var contacts = new List<Contact> { 
+            new Contact { Name="John Doe", Email="john@yahoo.com"} ,
+             new Contact { Name="Jane Doe", Email="jane@yahoo.com"}
+        };
+
+        List<string> list = new List<string>()
+        {
+            "John Doe",
+            "Janie Doe",
+            "Tom Hanks",
+            "Frank Liu"
+        };
+        listContacts.ItemsSource = contacts;
 	}
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
 
     private void btnEditContact_Clicked(object sender, EventArgs e)
     {
@@ -15,5 +36,10 @@ public partial class ContactsPage : ContentPage
     private void btnAddContact_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync(nameof(AddContactPage));
+    }
+
+    private void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+
     }
 }
